573e425f64d501fb45c078b76caa6a0b7f2dff81
João Alves <138532454+jonh213412421@users.noreply.github.com>
2024-02-24 21:39:36 -0300

    Create Setup.py

---
+++ Setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+import setuptools
+
+setup(
+    name='CryptP',
+    version='1.0',
+    packages=setuptools.find_packages(),
+    url='',
+    license='',
+    author='João Bajur',
+    author_email='joaobajur@hotmail.com',
+    description='Encrypts files'
+)
